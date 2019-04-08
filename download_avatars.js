d8254b46035425d139c8886624c24562cda13f69
Thomas Wenner <thmswenner@gmail.com>
2019-04-08 23:57:38 +0000

    added files to ignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,26 @@
 var request = require('request');
+var secrets = require('secrets');
 
 console.log('Welcome to the Github Avatar Downloader!!')
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers : {
+      'User-Agent' : 'request',
+      'Authorization' :
+    }
+  };
 
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+//febcadf72f114a6bc58ff6ee343e0ebfad0a2468
