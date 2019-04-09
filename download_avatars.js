3226515908765fee0811edbf5aa367b1394a3f6a
Thomas Wenner <thmswenner@gmail.com>
2019-04-09 00:44:53 +0000

    Created for loop to run through parsed body

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var secrets = require('secrets');
+var secrets = require('./secrets.js');
 
 console.log('Welcome to the Github Avatar Downloader!!')
 
@@ -7,18 +7,21 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers : {
-      'User-Agent' : 'request',
-      'Authorization' :
+      'User-Agent': 'request',
+      'Authorization': 'Bearer ' +secrets.GITHUB_TOKEN
     }
   };
 
-  request(url, function(err, res, body) {
-    cb(err, body);
+  request(options, function(err, res, body) {
+      var parsed = JSON.parse(body);
+      for (var element of parsed) {
+        cb(err, element.avatar_url);
+      }
   });
 }
 
 
+
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
   console.log("Result:", result);
 });
